9199b30ee7a018fea542b26b3c8017bbdfa5ab5f
Cmd <554120271@qq.com>
2016-09-16 15:41:32 +0800

    页面已玩坏。。。

--- index.js
+++ index.js
@@ -56,4 +56,76 @@ $(function() {
       state = 0;
     }
   }, 1050);
+  $('div.rect span').each(function() {
+    var tar = this;
+    setInterval(function() {
+      var r = parseInt(Math.random() * 150) + 100;
+      var g = parseInt(Math.random() * 150) + 100;
+      var b = parseInt(Math.random() * 150) + 100;
+      $(tar).css('border-color', 'rgb(' + r + ', ' + g + ', ' + b + ')');
+    }, 1000);
+  });
+  var lastX;
+  var lastY;
+  $('div.rect').on('mousedown', function(e) {
+    lastX = e.clientX;
+    lastY = e.clientY;
+    $('body').on('mousemove', rectMove);
+  });
+  $('body').on('mouseup', function() {
+    $('body').off('mousemove');
+  });
+  function rectMove(e) {
+    var nowX = e.clientX;
+    var nowY = e.clientY;
+    var orgL = parseInt($('div.rect').css('left').slice(0, -2));
+    var orgB = parseInt($('div.rect').css('bottom').slice(0, -2));
+    $('div.rect').css('left', (orgL + nowX - lastX) + 'px');
+    $('div.rect').css('bottom', (orgB - nowY + lastY) + 'px');
+    lastX = nowX;
+    lastY = nowY;
+  }
+  setInterval(function() {
+    var cOe = Math.random() < 0.3;
+    var tempText = '';
+    if (cOe) {
+      tempText = (Math.round(Math.random() * 20901) + 19968).toString(16);
+      tempText = eval('\'\\u' + tempText + '\'');
+    } else {
+      var mOe = Math.random() < 0.5;
+      if (mOe) {
+        tempText = Math.floor(Math.random() * 10);
+      } else {
+        var lOu = Math.random() < 0.5;
+        var ser = Math.floor(Math.random() * 26);
+        if (lOu) {
+          tempText = String.fromCharCode(97 + ser);
+        }else {
+          tempText = String.fromCharCode(65 + ser);
+        }
+      }
+    }
+    var fontSize = 30 + Math.floor(Math.random() * 40);
+    var randomColor = Math.random();
+    if (randomColor < 0.33) {
+      randomColor = '#6699ff';
+    } else if (randomColor < 0.66) {
+      randomColor = '#ff6699';
+    } else {
+      randomColor = '#99ff66';
+    }
+    var tempLeft = Math.random() * 100;
+    var tempTime = Math.random() * 3 + 2;
+    var tempSpan = $('<span>').html(tempText).css({
+        'font-size': fontSize + 'px',
+        'color': randomColor,
+        'left': tempLeft + '%',
+        'animation': 'textDown ' + tempTime + 's linear',
+        '-webkit-animation': 'textDown ' + tempTime + 's linear'
+      });
+    setTimeout(function() {
+      tempSpan.remove();
+    }, tempTime * 1000 + 100);
+    $('div.bg').append(tempSpan);
+  }, 100);
 });
